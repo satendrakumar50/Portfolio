1aab8520bda56edf1fdeb4e1dc5c3f56dfc13e30
satendrakumar50 <gk1293123@gmail.com>
2025-10-03 12:42:25 +0530

    third commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     
     >
       <div className="About-me">
-       
+        <h1>About Me</h1>
         <p>
           I am a dedicated and results-driven software developer with a strong
           foundation in computer science and a passion for creating efficient
